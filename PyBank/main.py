13d6b62b266c6a5f1cb871d94ed902c53a876323
jessicaholmes1190-wpaZMo <jessicaholmes1190@gmail.com>
2024-04-16 12:41:48 -0500

    updating

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,12 +4,9 @@ import csv
 
 csv_path = "PyBank\\Resources\\budget_data.csv"
 
-
 #open csv
 csvfile = open(csv_path, mode = 'r')
-csvreader =csv.reader(csvfile)
+csvreader =csv.reader(csvfile, delimiter=',')
 
 for row in csvreader:
       print(row)
-
-      
